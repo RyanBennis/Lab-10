e136962155b1bc14fa225e2bbf4c4cc8a67cd083
Ryan.bennis <Buddybot180@gmail.com>
2021-06-23 16:52:56 -0400

    Add project files.

---
+++ Lab 10/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10
+{
+    public class Movie
+    {
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+
+        public Movie()
+        {
+        }
+
+        public Movie(string title, string category)
+        {
+
+            Title = title;
+            Category = category;
+
+        }
+
+
+    }
+}
---
+++ Lab 10/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string continueFlag;
+            do
+            {
+
+
+
+                var movies = new List<Movie>()
+            {
+
+                new Movie(){Title = "Luca", Category= "Animated" },
+                new Movie(){Title = "Steam Boat Willy", Category= "Animated" },
+                new Movie(){Title = "Dune", Category= "Sci-Fi" },
+                new Movie(){Title = "Back to the Future", Category= "Sci-fi" },
+                new Movie(){Title = "Texas Chainsaw Massacer", Category= "Horror" },
+                new Movie(){Title = "Get Out", Category= "Horror" },
+                new Movie(){Title = "The Big Sick", Category= "Drama" },
+                new Movie(){Title = "Avengers Endgame", Category= "Drama" },
+                new Movie(){Title = "Blade Runner", Category= "Sci-Fi" },
+                new Movie(){Title = "Captain America: The First Avenger", Category= "Drama" },
+
+            };
+
+            
+                 listofMovies(movies);
+
+
+                Console.WriteLine("Do you want to continue (y/n)");
+                continueFlag = Console.ReadLine();
+
+            } while (continueFlag == "y");
+
+        }
+
+
+        static string UserInput(string message)
+        {
+
+            string input;
+            do
+            {
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+
+                    Console.WriteLine("Not a Valid Category");
+
+                }
+
+
+            } while (string.IsNullOrWhiteSpace(input));
+            return input;
+        }
+
+        public static void listofMovies(List<Movie> movies)
+
+        {
+            string input = UserInput("What is the Category you are interested in \r\n 1.Drama \r\n 2.Animated \r\n 3.Sci-Fi \r\n 4.Horror");
+            var results = movies.Where(movie => movie.Category == input);
+
+
+            foreach (var movie in results)
+            {
+                Console.WriteLine($"{(movie.Title)}");
+
+            }
+
+
+        }
+
+
+
+    }
+}
